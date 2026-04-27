e9e607781b677020190d083f30318ec62c433075
Ovidio <romeroovidio@yahoo.com>
2026-04-26 20:47:26 -0500

    agregar notificaciones push

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {});
 
-// 🔔 ESCUCHAR NOTIFICACIONES
+// 🔔 PUSH NOTIFICATIONS
 self.addEventListener("push", event => {
 
   const data = event.data ? event.data.json() : {
@@ -12,9 +12,21 @@ self.addEventListener("push", event => {
     body: "Nueva tarea disponible"
   };
 
-  self.registration.showNotification(data.title, {
+  const options = {
     body: data.body,
     icon: "/logo.png"
-  });
+  };
 
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+// 👆 CLICK EN NOTIFICACIÓN
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("/")
+  );
 });
